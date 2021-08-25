104fba162fce1dced4ba9aefc45d6ab0dd8cd40e
kasata373 <falken783@gmail.com>
2021-08-25 12:53:15 +0900

    商品一覧表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,19 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_item, only: [:edit, :update, :show, :destroy]
+
 
   def move_to_index
     redirect_to new_user_session_path unless user_signed_in?
   end
 
   def index
+    @item = Item.all
     # @item = Item.order("created_at DESC")
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   def new
@@ -28,4 +35,5 @@ class ItemsController < ApplicationController
    def item_params
      params.require(:item).permit(:name, :product, :category_id, :state_id, :postage_id, :prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
    end
+
 end
